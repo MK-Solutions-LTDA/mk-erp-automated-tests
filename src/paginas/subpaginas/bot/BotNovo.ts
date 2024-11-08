b7195c28415dc43bf7dba7109b4fefdd272ed87a
carolinemmk <caroline.mendel@mksolutions.com.br>
2024-11-08 17:17:37 -0300

    .........

--- src/paginas/subpaginas/bot/BotNovo.ts
+++ src/paginas/subpaginas/bot/BotNovo.ts
@@ -3,9 +3,9 @@ import step from "../../../utilitarios/decorators";
 import { expect } from "../../../utilitarios/fixtures/base";
 
 export default class BotNovo {
-    
+
     page: Page;
-    
+
     statusOperador: Locator;
     indicadoresMenu: Locator;
     chatBotMenu: Locator;
@@ -29,6 +29,16 @@ export default class BotNovo {
     checkboxSelecionarPrimeiraFatura: Locator;
     encerrarConversaDefinitivamente: Locator;
     botaoEnviarSegundaViaFaturas: Locator;
+    botaoAcoesDentroConversaRespostasPadrao: Locator;
+    botaoAcoesDentroConversaRespostasPadraoCriar: Locator;
+    botaosalvarRespostaPadrao: Locator;
+    botaoFecharMenuRespostasPadrao: Locator;
+    cadastroGrupoRespostaPadrao: Locator;
+    cadastroMensagemRespostaPadrao: Locator;
+    botaoConversaOpcoesRedefinirCliente: Locator;
+    botaoConversaOpcoesRedefinirClienteBuscarCliente: Locator;
+    botaoConversaOpcoesRedefinirClienteSalvar: Locator;
+    botaoConversaOpcoesRedefinirClienteSalvarConfirma: Locator;
 
     constructor(page: Page) {
 
@@ -50,7 +60,7 @@ export default class BotNovo {
         this.exportarCSV = this.page.getByRole('button', { name: 'Exportar CSV' });
 
         this.botaoConversaOpcoes = this.page.getByRole('button', { name: 'Opções' });
-        this.botaoConversaOpcoesEncerrarAtendimento = this.page.getByText('Encerrar atendimento' );
+        this.botaoConversaOpcoesEncerrarAtendimento = this.page.getByText('Encerrar atendimento');
         this.confirmarEncerramentoConversa = this.page.getByLabel('Estou ciente que esta ação nã')
         this.encerrarConversaDefinitivamente = this.page.getByRole('button', { name: 'Encerrar' });
         this.botaoNovaConversa = this.page.locator('xpath=//div[@id="radix-:r11:-content-clients"]//button[1]').nth(0);
@@ -58,6 +68,16 @@ export default class BotNovo {
         this.botaoAcoesDentroConversaEnviarSegundaVia = this.page.getByRole('button', { name: 'Enviar 2ª via de fatura' }).first();
         this.checkboxSelecionarPrimeiraFatura = this.page.locator('td').first()
         this.botaoEnviarSegundaViaFaturas = this.page.getByRole('button', { name: 'Enviar (1)' });
+        this.botaoAcoesDentroConversaRespostasPadrao = this.page.getByRole('button', { name: 'Respostas Padrão' });
+        this.botaoAcoesDentroConversaRespostasPadraoCriar = this.page.getByRole('button', { name: 'clique aqui' });
+        this.botaosalvarRespostaPadrao = this.page.getByRole('button', { name: 'Salvar' });
+        this.cadastroGrupoRespostaPadrao = this.page.getByPlaceholder('Clique para preencher');
+        this.cadastroMensagemRespostaPadrao = this.page.getByLabel('Mensagem *');
+        this.botaoFecharMenuRespostasPadrao = this.page.locator('[id="radix-\\:rov\\:"] > .w-12').nth(0);
+        this.botaoConversaOpcoesRedefinirCliente = this.page.getByText('Redefinir cliente');
+        this.botaoConversaOpcoesRedefinirClienteBuscarCliente = this.page.getByPlaceholder('Clique para buscar por código');
+        this.botaoConversaOpcoesRedefinirClienteSalvar = this.page.getByRole('button', { name: 'Redefinir cliente' });
+        this.botaoConversaOpcoesRedefinirClienteSalvarConfirma = this.page.getByRole('button', { name: 'Confirmar' });
     };
 
     @step('Limpar conversa')
@@ -65,7 +85,7 @@ export default class BotNovo {
         await this.botaoConversaOpcoes.click();
         await this.botaoConversaOpcoesEncerrarAtendimento.click();
         await this.confirmarEncerramentoConversa.click();
-        await this.encerrarConversaDefinitivamente.click();      
+        await this.encerrarConversaDefinitivamente.click();
     }
 
     @step('Abrir nova conversa')
@@ -101,5 +121,41 @@ export default class BotNovo {
         await this.checkboxSelecionarPrimeiraFatura.click();
         await this.botaoEnviarSegundaViaFaturas.click();
         await expect(this.page.locator('div').filter({ hasText: /^Sucesso!Segunda via enviada com sucesso!$/ }).nth(2)).toBeVisible();
+    }
+
+    @step('Criar resposta padrão')
+    async criarResposta() {
+        await this.botaoAcoesDentroConversa.click();
+        await this.botaoAcoesDentroConversaRespostasPadrao.click();
+        await this.botaoAcoesDentroConversaRespostasPadraoCriar.click();
+        await this.cadastroGrupoRespostaPadrao.click();
+        await this.cadastroGrupoRespostaPadrao.pressSequentially('Teste automação');
+        await this.cadastroMensagemRespostaPadrao.click();
+        await this.cadastroMensagemRespostaPadrao.pressSequentially('Olá #nome! Este é um teste de resposta padrão com acentuação, coringa e emoji 😉😊💚🥰💛😅. Você está recebendo seu protocolo que é: #protocolo.');
+        await this.botaosalvarRespostaPadrao.click();
+        await expect(this.page.locator('div').filter({ hasText: /^Sucesso!Mensagem padrão criada com sucesso!$/ }).nth(2)).toBeVisible();
+        //await this.botaoFecharMenuRespostasPadrao.click();
+    }
+
+    @step('Redefinir contato já identificado')
+    async redefinirContato() {
+        await this.botaoConversaOpcoes.click();
+        await this.botaoConversaOpcoesRedefinirCliente.click();
+        await this.botaoConversaOpcoesRedefinirClienteBuscarCliente.click();
+        await this.page.getByPlaceholder('Clique para buscar por código').click();
+        await this.page.getByPlaceholder('Clique para buscar por código').fill('teste banco de dados');
+        await this.page.getByText('teste banco dados').first().click();
+        await this.botaoConversaOpcoesRedefinirClienteSalvar.click();
+        await this.botaoConversaOpcoesRedefinirClienteSalvarConfirma.click();
+        await expect(this.page.locator('div').filter({ hasText: /^Sucesso!O contato foi redefinido!$/ }).nth(2)).toBeVisible();
+        await this.botaoConversaOpcoes.click();
+        await this.botaoConversaOpcoesRedefinirCliente.click();
+        await this.botaoConversaOpcoesRedefinirClienteBuscarCliente.click();
+        await this.page.getByPlaceholder('Clique para buscar por código').click();
+        await this.page.getByPlaceholder('Clique para buscar por código').fill('teste caroline');
+        await this.page.getByText('teste caroline').first().click();
+        await this.botaoConversaOpcoesRedefinirClienteSalvar.click();
+        await this.botaoConversaOpcoesRedefinirClienteSalvarConfirma.click();
+        await expect(this.page.locator('div').filter({ hasText: /^Sucesso!O contato foi redefinido!$/ }).nth(2)).toBeVisible();
     }
 }
--- testes/bot/bot_novo.spec.ts
+++ testes/bot/bot_novo.spec.ts
@@ -9,5 +9,24 @@ test.describe('Ações', () => {
         await page.waitForTimeout(2000);
         await paginaBotNovo.enviarAnexo();
     });
-    
+
+    test('Criar resposta padrão', async ({ paginaLogin, paginaPrincipal, paginaBotNovo, page }) => {
+        await page.waitForLoadState('load')
+        await paginaBotNovo.abrirNovaConversa();
+        await page.waitForTimeout(2000);
+        await paginaBotNovo.acessarConversa();
+        await page.waitForTimeout(2000);
+        await paginaBotNovo.criarResposta();
+    });
+});
+
+test.describe('Opções', () => {
+    test('Redefinir contato identificado', async ({ paginaLogin, paginaPrincipal, paginaBotNovo, page }) => {
+        await page.waitForLoadState('load')
+        await paginaBotNovo.abrirNovaConversa();
+        await page.waitForTimeout(2000);
+        await paginaBotNovo.acessarConversa();
+        await page.waitForTimeout(2000);
+        await paginaBotNovo.redefinirContato();
+    });
 });
