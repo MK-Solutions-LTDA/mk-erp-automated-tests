2f7f5df4ef5729824ac4dcf0a38b64148471907a
marcokist <marco.kist@mksolutions.com.br>
2024-11-11 07:44:35 -0300

    Removendo Setup.ts do controle de versão

--- Setup.ts
+++
@@ -1,3 +0,0 @@
-export const URL = process.env.npm_config_url || 'https://mk4.mksolutions.com.br';
-export const user = process.env.npm_config_username || 'caroline';
-export const pass = process.env.npm_config_senha || 'Mk123456';
